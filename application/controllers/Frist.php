8f393f73043e1e5a9c63bba0e42e27ce293ff2bb
Shimura <quachtothinh@gmal.com>
2017-12-02 10:24:30 +0700

    database update select delete

--- application/controllers/Frist.php
+++ application/controllers/Frist.php
@@ -5,6 +5,7 @@ class Frist extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->database();
 	}
 
 	public function index()
@@ -23,6 +24,40 @@ class Frist extends CI_Controller {
 		$this->load->view('frist');		
 	}
 
+	public function addData()
+	{
+		$data = array(
+			'roll_no' => '1',
+			'Name' => 'Thinh'
+		);
+		$this->db->insert('stud', $data);
+		echo "Da add data thanh cong";		
+	}
+	public function updateData()
+	{
+		$data = array(
+			'roll_no' => '1',
+			'Name' => 'Jack'
+		);
+		$this->db->set($data);
+		$this->db->where('roll_no', '1');
+		$this->db->update("stud", $data);
+		echo "Update thanh cong";
+	}
+	public function deleteData()
+	{
+		$this->db->delete('stud', "roll_no = 1");
+		echo "Xoa thanh cong";
+	}
+	public function getData()
+	{
+		$query = $this->db->get('stud');
+		$data['records'] = $query->result();
+	}
+	public function __destruct()
+	{
+		$this->db->close();
+	}
 }
 
 /* End of file Frist.php */

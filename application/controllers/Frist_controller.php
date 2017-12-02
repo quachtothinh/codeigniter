e26a84eea5e8b43526d683a87c3db9c1291c8b33
Shimura <quachtothinh@gmal.com>
2017-12-02 09:35:52 +0700

    hello word show controller

---
+++ application/controllers/Frist_controller.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Frist_controller extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		echo "Hello world Codeigniter";
+	}
+
+}
+
+/* End of file Frist.php */
+/* Location: ./application/controllers/Frist.php */
